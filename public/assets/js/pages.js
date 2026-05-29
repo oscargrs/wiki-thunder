c56128c751a11af3b42c0be863f2874bc5fb9d9d
Oscar Silva <oscargomesribeiro@gmail.com>
2026-05-29 15:15:51 -0300

    Mudei uns bgl

--- public/assets/js/pages.js
+++ public/assets/js/pages.js
@@ -4,11 +4,14 @@ function validarSessao() {
 
     var alistarSpan = document.getElementById("alistarSpan");
     var alistarRedirect = document.getElementById("alistarRedirect");
+    var b_usuario = document.getElementById("b_usuario");
+    var userInfo = document.getElementById("userInfo");
 
     if (email != null && nome != null) {
         alistarSpan.innerHTML = "DASHBOARD";
-        alistarRedirect.href = "../../dashboard/dash-visao-geral.html"
+        alistarRedirect.href = "../../dashboard/dash-visao-geral.html";
+        b_usuario.innerHTML = nome;
     } else {
-
+        userInfo.style.display = "none";
     }
 }
